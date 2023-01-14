170068452abc90ca3d843953f9556f2e771cc10e
Cecato <gustavocecato010620@gmail.com>
2023-01-14 20:28:49 -0300

    fetch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 import {Header} from '../../components/Header';
 import background from "../../assets/background.png"
+import ItemList from '../../components/ItemList';
 import './styles.css';
 
 function App() {
+    const [user, setUser] = useState('');
+    const [currentUser, setCurrentUser] = useState(null);
+    const [repos, setRepos] = useState(null);
+
+    const handleGetData = async () => {
+        const userData = await fetch(`https://api.github.com/users/${user}`);
+        const newUser = await userData.json();
+
+        if(newUser.name){
+            const {avatar_url, name, bio} = newUser;
+            setCurrentUser({avatar_url, name, bio});
+
+            const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+            const newRepos = await reposData.json();
+
+            if(newRepos.length){
+                setRepos(newRepos);
+            }
+        }
+        
+    }
+
     return (
         <div className="App">
             <Header/>
@@ -12,22 +36,55 @@ function App() {
 
                 <div className='info'>
                     <div>
-                        <input name='usuario' placeholder='@username'/>
-                        <button>Buscas</button>
+                        <input 
+                            name='usuario' 
+                            placeholder='@username'
+                            value={user}
+                            onChange={event => setUser(event.target.value)}
+                            />
+                        <button onClick={handleGetData}>Buscas</button>
                     </div>
 
-                    <div className='perfil'>
-                        <img 
-                            src="https://avatars.githubusercontent.com/u/69871760?v=4" 
-                            className='profile' 
-                            alt='profile'/>
+                    {currentUser?.name ?(
+                        <>
+                        <div className='perfil'>
+                            <img 
+                                src="https://avatars.githubusercontent.com/u/69871760?v=4" 
+                                className='profile' 
+                                alt='profile'/>
 
-                        <div>
-                            <h3>Gustavo Cecato</h3>
-                            <span>@gu_cecato</span>
-                            <p>Descrição</p>
+                            <div>
+                                <h3>Gustavo Cecato</h3>
+                                <span>@gu_cecato</span>
+                                <p>Descrição</p>
+                            </div>
                         </div>
-                    </div>
+                        <hr/>
+                        </>
+                        
+                    ): null}
+
+
+
+                    {repos?.length ?(
+                        <>
+                        <div>
+                            <h4 className='repositorio'>Repositório</h4>
+                            <ItemList 
+                                title="teste1" 
+                                description="teste1"/>
+                            <ItemList 
+                                title="teste1" 
+                                description="teste1"/>
+                            <ItemList 
+                                title="teste1" 
+                                description="teste1"/>
+                            <ItemList 
+                                title="teste1" 
+                                description="teste1"/>
+                        </div>
+                        </>
+                    ): null}
 
                 </div>
                 
